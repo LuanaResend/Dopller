c7b62e69e6052eeef2510622210d73dfdcc51efe
LuanaResend <113730688+LuanaResend@users.noreply.github.com>
2023-07-21 16:46:18 -0300

    Add files via upload

---
+++ dopller/dashboard.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php include "header.php";?>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="dashboard.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&family=Open+Sans&display=swap" rel="stylesheet">
+
+    <title>Dopller</title>
+</head>
+<body>
+    <section class="content">
+        
+    <div class="content-1">
+        <h1>Desperte o Potencial do Seu Negócio com o Dopller</h1>
+        <p>O PCP, ou Planejamento e Controle de Produção, é a espinha dorsal de qualquer empresa que busca eficiência e sucesso. É o processo estratégico que engloba desde o planejamento das atividades até o controle preciso dos recursos, garantindo que tudo funcione perfeitamente em harmonia.</p>
+        <ion-icon name="arrow-down-outline"></ion-icon>
+    </div>
+
+    
+    <div class="content-2">
+        <h1>Dopller: A Transformação Exponencial do Planejamento e Controle de Produção</h1>
+        <p>E agora, com o Dopller, você tem a ferramenta completa para otimizar e aprimorar seu PCP. Ele é o parceiro ideal para gerenciar todas as etapas do ciclo de produção, desde o planejamento e agendamento até a execução e monitoramento, tudo em um só lugar.</p>
+        <ion-icon name="arrow-down-outline"></ion-icon>
+    </div>
+
+    <div class="content-2">
+        <h1>Interface Inovadora: Simplificando o Planejamento e Controle de Produção</h1>
+        <p>Com o Dopller, você terá acesso a recursos avançados, como previsão de demanda precisa, otimização de estoques, alocação inteligente de recursos, rastreamento em tempo real e análise de desempenho detalhada. Tudo isso para impulsionar a eficiência, reduzir custos e aumentar a produtividade.</p>
+        <ion-icon name="arrow-down-outline"></ion-icon>
+    </div>
+
+    <div class="content-2">
+        <h1>Dopller em Ação: Resultados Impressionantes para Empresas como a Sua</h1>
+        <p>Imagine o poder de ter todas as informações e ferramentas necessárias para tomar decisões estratégicas informadas, reduzindo o tempo de resposta e impulsionando sua vantagem competitiva. Com o Dopller, você pode alcançar um novo patamar de excelência operacional.</p>
+        <ion-icon name="arrow-down-outline"></ion-icon>
+    </div>
+
+    <div class="content-3">
+    <p>Ready to Accelerate your Business</p>
+    </div>
+    </section>
+
+    
+    
+</body>
+</html>
---
+++ dopller/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header.css">
+    <link rel="import" href="dashboard.html">
+    <title>Dopller</title>
+</head>
+<body>
+
+    <header>
+        <h1>Dopller</h1>
+        <a href="#" target="_blank" rel="external"><button><p>Login | Sign-in</p></button></a>
+    </header>
+
+    <input type="checkbox" id="checkbox"> 
+<label class="btn" for="checkbox"><i class="fa fa-bars"></i> <span><ion-icon name="menu-outline"></ion-icon></span></label >
+    
+
+<div class="sidebar">
+    <ul>
+        <a href="#"><li><i class="fa fa-lightbulb-o"></i>About</li></a>
+        <a href="#" class="active"><li><i class="fa fa-line-chart"></i>Frequently Asked</li></a>
+        
+        <a href="#"><li><i class="fa fa-paint-brush"></i>(00) 0000-0000</li></a>
+        <a href="#"><li><i class="fa fa-file-text-o"></i>(00) 0000-0000</li></a>
+        <div class="contact">
+        <a href="#"><li><i class="fa fa-television"></i><ion-icon name="logo-whatsapp"></ion-icon></li></a>
+            <a href="#"><li><i class="fa fa-send"></i><ion-icon name="logo-instagram"></ion-icon></li></a>
+            <a href="#"><li><i class="fa fa-wpforms"></i><ion-icon name="logo-facebook"></ion-icon></li></a> </div>
+    </ul>
+</div> 
+    
+<script src="script.js"></script>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+</body>
+</html>
